7910ae34479aeb46b660e30a3925e41968b836a6
jmajorNREL <joshua.major@nrel.gov>
2017-07-24 12:32:04 -0600

    Stand along autoGUI with ability to save steps

--- autoGui.py
+++ autoGui.py
@@ -1,123 +1,184 @@
-#import modules
+#imports modules
 import pyautogui as auto
+import datetime
+import time
 import pickle
 
+
+#Pushed from my NREL computer
+
+#allows for a failsafe to break the program
 auto.PAUSE = 1
 auto.FAILSAFE = True
 
+print('\n\n\n')
+print('  Welcome to autoGUI.  '.center(240,'#'))
 
-
-print("\n", "Welcome to autoGUI. This program will alow you to automate".center(80), "anything you can do with the mouse or keyboard.".center(80),"\n", "To cancel the program, move the mouse to the top left".center(80), "corner of the screen".center(80))
-
-#This is the class used in each step of the program
-class automate:
-    
-    location = 0 #stores location for movement function
-    duration = 2 #the duration in seconds for the mouse to move locations
-    message = 'print' #the default message for the type function
-    move = 0     #uses the move function when value is changed to 1
-    click = 0    #uses the click function when value is changed to 1
-    pri = 0      #uses the print function when value is changed to 1
-    enter = 0    #uses the enter function when the value is changed to 1 
-    
-    #this definition contains all the functions 
-    def doStuff(self):
-        if self.move == 1:
-            auto.moveTo(self.location, duration = self.duration)
-            
-            
-        elif self.click == 1:
-            auto.click()
-            
-        elif self.pri == 1:
-            auto.typewrite(self.message, interval = .1)
-            print(self.message)
-                
-        elif self.enter == 1:
-            auto.hotkey('enter')
-            
-
-#this variable will contain all the step objects    
-step = []        
-
-rs = 0 #variable used to initiate the loading a saved profile
-  
-#This loop alows the user to enter multiple steps
 while True:
+    #sets all the parameters for the program loop
+    ##########################################################################start
     
-    step.append(automate()) #creates a new object for each step at the begining of the loop
-    
-    x = input('What would you like to do with this step: \n1=set location \n2=left click, \n3=type a message, \n4=hit enter, \ns=save steps, \nrs= Run saved steps, \n9=runs steps\n')
-    
-    
-    #this if block changes the variables in the object to determin which function is run
-    if x == '1':
-        step[len(step) - 1].location = auto.position()
-        step[len(step) - 1].move = 1
-        print(step[len(step) - 1].location)
-    
-    elif x == '2':
-        step[len(step) - 1].click = 1
-    
-    elif x == '3': 
-        step[len(step) -1].message = input('Enter a message to print: ')
-        step[len(step) - 1].pri = 1
-    
-    elif x == '4': 
-        step[len(step) - 1].enter = 1
+   
 
-    elif x == 's':
-        with open('company_data.pkl', 'wb') as output:
-            for g in range(len(step)-1):
-                pickle.dump(step[g], output, pickle.HIGHEST_PROTOCOL)
+    #creates a class to be used for each step of the SSTDR program
+    class automate:
+        
+                location = 0 #stores location for movement function
+                duration = 1 #the duration in seconds for the mouse to move locations
+                message = 'print' #the default message for the type function
+                move = 0     #uses the move function when value is changed to 1
+                click = 0    #uses the click function when value is changed to 1
+                doubleclick = 0    #uses the click function when value is changed to 1
+                dateTime = 0
+                pri = 0      #uses the print function when value is changed to 1
+                enter = 0    #uses the enter function when the value is changed to 1 
+                ctrlA = 0
+                ctrlC = 0
+                ctrlV = 0
+                progPause = 0
+                sleeper = 1
                 
-    elif x == 'rs':
-        rs = 1
-        break
-        
+                #this definition contains all the functions 
+                def doStuff(self):
+                    if self.move == 1:
+                        auto.moveTo(self.location, duration = self.duration) 
+                        auto.click()
+                        
+                    elif self.doubleclick == 1:
+                        auto.moveTo(self.location, duration = self.duration)
+                        auto.click(clicks=2)
+                        
+                    elif self.pri == 1:
+                        auto.typewrite(self.message, interval = .1)
+                        print(self.message)
+                        
+                    elif self.dateTime == 1:
+                        fileDate = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+                        auto.typewrite('file_%s' %(fileDate), interval = .1) #writes file with date, time, and temperature the test was run at
+                            
+                    elif self.enter == 1:
+                        auto.hotkey('enter')
+                        
+                    elif self.ctrlA == 1:
+                        auto.hotkey('ctrl', 'a')
+                        
+                    elif self.ctrlC == 1:
+                        auto.hotkey('ctrl', 'c')
+                        
+                    elif self.ctrlV == 1:
+                        auto.hotkey('ctrl', 'v')
+                        
+                    elif self.progPause == 1:
+                        time.sleep(int(self.sleeper))
+                        
+    
+    
+    step = [] #this variable will contain all the step objects    
+    rs = 0 #used to run saved steps
+    
+    #This loop alows the user to enter multiple steps
+    while True:
+                
+        step.append(automate()) #creates a new object for each step at the begining of the loop
+                
+        x = input('\nWhat would you like to do with this step: \n1 = Move to and click a location, \n2 = Type a message, \n3 = Hit enter, \n4 = Move to and double click a location, \n5 = Prints a file name with the date and time, \n6 = ctrl a, \n7 = ctrl c, \n8 = ctrl v, \n9 = Pause, \ns = Save steps, \nrs = Run saved steps, \nr = Run steps\n')
+                
+                
+                #this if block changes the variables in the object to determin which function is run
+        if x == '1':
+            step[len(step) - 1].location = auto.position()
+            step[len(step) - 1].move = 1
+            print(step[len(step) - 1].location)
+                
+        elif x == '2': 
+            step[len(step) -1].message = input('Enter a message to print: ')
+            step[len(step) - 1].pri = 1
+                
+        elif x == '3': 
+            step[len(step) - 1].enter = 1
+                         
+        elif x == '4': 
+            step[len(step) - 1].location = auto.position()
+            step[len(step) - 1].doubleclick = 1
+                         
+        elif x == '5': 
+            step[len(step) - 1].dateTime = 1
+    
+        elif x == '6': 
+            step[len(step) - 1].ctrlA = 1
+    
+        elif x == '7': 
+            step[len(step) - 1].ctrlC = 1
+    
+        elif x == '8': 
+            step[len(step) - 1].ctrlV = 1
+                         
+        elif x == '9': 
+            step[len(step) - 1].progPause = 1
+            step[len(step) - 1].sleeper = input('Enter the amount of time to pause: ')
             
-    #breaks the loop when the user wants to run the steps         
-    elif x == '9':
-        break
-
-    else:
-        print('Please select a valid option')
-        
-if rs !=1:        
-    l = input('How many times would you like these steps to run?\n') or '1'
-
-
-while True and rs != 1:    
-    for n in range(int(l)): #determines how many times the steps are run
-        for i in range(int(len(step) - 1)): #this loop call each step object sequentially
-            print("Step:", i + 1,",", "Loop count:" , n+1, ",", "Move: ", step[i].move,",", "Click: ", step[i].click,",", "print: ", step[i].pri,",", 'enter: ', step[i].enter )
-            step[i].doStuff() 
-    br = input('Would you like to run the steps again: y/n')
-    if br == 'n':
-        break
-
-#used to run saved profile
-if rs == 1:
-    redo = input('How many times would you like these steps to run?\n' ) or 1
-    input('Press enter to start saved profile\n')
-    for q in range(int(redo)):
-      
-        stepsFromSaved = []
-        b = 0
-        with open('company_data.pkl', 'rb') as i:
-            while True:
-                    
-                stepsFromSaved.append(None)
-                try:
-                    stepsFromSaved[b] = pickle.load(i)
-                except EOFError:
-                       break
-                b += 1
-                    
-            for g in range(len(stepsFromSaved) - 1):
-                stepsFromSaved[g].doStuff()
-
+        elif x == 's':
+            with open('company_data.pkl', 'wb') as output:
+                for g in range(len(step)-1):
+                    pickle.dump(step[g], output, pickle.HIGHEST_PROTOCOL)
+                
+        elif x == 'rs':
+            rs = 1
+            break
+                
+        #breaks the loop when the user wants to run the steps         
+        elif x == 'r':
+            break
+            
+        else:
+            print('\n\n**Please select a valid option**\n\n')
+    ############################################################################end
     
     
-input('Pres enter to exit')
+    
+    
+    
+    
+    
+    
+    if rs !=1:        
+        l = input('How many times would you like these steps to run?\n') or '1'
 
+
+    while True and rs != 1:    
+        for n in range(int(l)): #determines how many times the steps are run
+            for i in range(int(len(step) - 1)): #this loop call each step object sequentially
+                print("Step:", i + 1,",", "Loop count:" , n+1, ",", "Move: ", step[i].move,",", "Click: ", step[i].click,",", "print: ", step[i].pri,",", 'enter: ', step[i].enter )
+                step[i].doStuff() 
+        br = input('Would you like to run the steps again: y/n')
+        if br == 'n':
+            break
+    
+    #used to run saved profile
+    if rs == 1:
+        redo = input('How many times would you like these steps to run?\n' ) or 1
+        input('Press enter to start saved profile\n')
+        for q in range(int(redo)):
+          
+            stepsFromSaved = []
+            b = 0
+            with open('company_data.pkl', 'rb') as i:
+                while True:
+                        
+                    stepsFromSaved.append(None)
+                    try:
+                        stepsFromSaved[b] = pickle.load(i)
+                    except EOFError:
+                           break
+                    b += 1
+                        
+                for g in range(len(stepsFromSaved) - 1):
+                    stepsFromSaved[g].doStuff()
+                    
+                    
+    endProgram = input('\nWould you like to run the program again? y\\n: '.center(40))
+    if endProgram == 'n':
+        break
+                    
+print('\nEnd of program')     
+    
