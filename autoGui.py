b2af2eff90a53899ce8568392078527d793c4c27
jmajorNREL <joshua.major@nrel.gov>
2017-08-31 10:47:49 -0600

    Updated filename and added wait for image

--- autoGui.py
+++ autoGui.py
@@ -8,7 +8,7 @@ import pickle
 #Pushed from my NREL computer
 
 #allows for a failsafe to break the program
-auto.PAUSE = 1
+auto.PAUSE = .5
 auto.FAILSAFE = True
 
 print('\n\n\n')
@@ -24,18 +24,20 @@ while True:
     class automate:
         
                 location = 0 #stores location for movement function
-                duration = 1 #the duration in seconds for the mouse to move locations
+                duration = .3 #the duration in seconds for the mouse to move locations
                 message = 'print' #the default message for the type function
                 move = 0     #uses the move function when value is changed to 1
                 click = 0    #uses the click function when value is changed to 1
                 doubleclick = 0    #uses the click function when value is changed to 1
                 dateTime = 0
+                file_discription = 'none'
                 pri = 0      #uses the print function when value is changed to 1
                 enter = 0    #uses the enter function when the value is changed to 1 
                 ctrlA = 0
                 ctrlC = 0
                 ctrlV = 0
                 progPause = 0
+                image_found = 0
                 sleeper = 1
                 
                 #this definition contains all the functions 
@@ -49,12 +51,12 @@ while True:
                         auto.click(clicks=2)
                         
                     elif self.pri == 1:
-                        auto.typewrite(self.message, interval = .1)
+                        auto.typewrite(self.message, interval = .05)
                         print(self.message)
                         
                     elif self.dateTime == 1:
-                        fileDate = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-                        auto.typewrite('file_%s' %(fileDate), interval = .1) #writes file with date, time, and temperature the test was run at
+                        fileDate = datetime.datetime.now().strftime("%Y-%m-%d")
+                        auto.typewrite('%s__%s' %(fileDate, self.file_discription), interval = .05) #writes file with date, time, and temperature the test was run at
                             
                     elif self.enter == 1:
                         auto.hotkey('enter')
@@ -71,6 +73,18 @@ while True:
                     elif self.progPause == 1:
                         time.sleep(int(self.sleeper))
                         
+                    elif self.image_found == 1:
+                        while True:
+    
+                            if auto.locateOnScreen('img.png') != None:
+                                print('Image found')
+                                break
+                                
+                            else:
+                                print('image not found')
+                            time.sleep(2)
+                            
+                        
     
     
     step = [] #this variable will contain all the step objects    
@@ -81,7 +95,7 @@ while True:
                 
         step.append(automate()) #creates a new object for each step at the begining of the loop
                 
-        x = input('\nWhat would you like to do with this step: \n1 = Move to and click a location, \n2 = Type a message, \n3 = Hit enter, \n4 = Move to and double click a location, \n5 = Prints a file name with the date and time, \n6 = ctrl a, \n7 = ctrl c, \n8 = ctrl v, \n9 = Pause, \ns = Save steps, \nrs = Run saved steps, \nr = Run steps\n')
+        x = input('\nWhat would you like to do with this step: \n1 = Move to and click a location, \n2 = Move to and double click a location \n3 = Type a message, \n4 = Hit enter, \n5 = Prints a file name with the date and time, \n6 = ctrl a, \n7 = ctrl c, \n8 = ctrl v, \n9 = Pause, \n10 = locate image, \ns = Save steps, \nrs = Run saved steps, \nr = Run steps\n')
                 
                 
                 #this if block changes the variables in the object to determin which function is run
@@ -89,19 +103,21 @@ while True:
             step[len(step) - 1].location = auto.position()
             step[len(step) - 1].move = 1
             print(step[len(step) - 1].location)
-                
+            
         elif x == '2': 
+            step[len(step) - 1].location = auto.position()
+            step[len(step) - 1].doubleclick = 1
+                
+        elif x == '3': 
             step[len(step) -1].message = input('Enter a message to print: ')
             step[len(step) - 1].pri = 1
                 
-        elif x == '3': 
+        elif x == '4': 
             step[len(step) - 1].enter = 1
                          
-        elif x == '4': 
-            step[len(step) - 1].location = auto.position()
-            step[len(step) - 1].doubleclick = 1
                          
         elif x == '5': 
+            step[len(step) - 1].file_discription = input('Enter a file name: ')
             step[len(step) - 1].dateTime = 1
     
         elif x == '6': 
@@ -121,7 +137,10 @@ while True:
             with open('company_data.pkl', 'wb') as output:
                 for g in range(len(step)-1):
                     pickle.dump(step[g], output, pickle.HIGHEST_PROTOCOL)
-                
+        
+        elif x == '10': 
+            step[len(step) - 1].image_found = 1
+                    
         elif x == 'rs':
             rs = 1
             break
@@ -143,12 +162,12 @@ while True:
     
     if rs !=1:        
         l = input('How many times would you like these steps to run?\n') or '1'
-
+        input('Press enter to start profile\n')
 
     while True and rs != 1:    
         for n in range(int(l)): #determines how many times the steps are run
             for i in range(int(len(step) - 1)): #this loop call each step object sequentially
-                print("Step:", i + 1,",", "Loop count:" , n+1, ",", "Move: ", step[i].move,",", "Click: ", step[i].click,",", "print: ", step[i].pri,",", 'enter: ', step[i].enter )
+                print("Step:", i + 1,",", "Loop count:" , n+1, )
                 step[i].doStuff() 
         br = input('Would you like to run the steps again: y/n')
         if br == 'n':
@@ -174,6 +193,7 @@ while True:
                         
                 for g in range(len(stepsFromSaved) - 1):
                     stepsFromSaved[g].doStuff()
+                    print('Step: ', g+1, ' loop: ', q+1)
                     
                     
     endProgram = input('\nWould you like to run the program again? y\\n: '.center(40))
